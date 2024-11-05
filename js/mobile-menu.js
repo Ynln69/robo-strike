946b9f7de1a5da0b5e9929aa1847a823ba21ab62
Yana Lunina <yanalunina96@gmail.com>
2024-11-05 02:11:30 +0200

    add content

---
+++ js/mobile-menu.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+  document.querySelector(".navbar").classList.toggle("active");
+}
+
+function closeMenu() {
+  document.querySelector(".navbar").classList.remove("active");
+}
